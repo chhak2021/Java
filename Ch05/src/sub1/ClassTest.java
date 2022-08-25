0329661a257e582d1bb434c2584968479f063882
chhak0503 <chhak0503@gmail.com>
2022-08-25 16:36:20 +0900

    2022/08/25 Ch05 수업예제 추가

--- Ch05/src/sub1/ClassTest.java
+++ Ch05/src/sub1/ClassTest.java
@@ -4,6 +4,10 @@ package sub1;
  * 날짜 : 2022/08/25
  * 이름 : 김철학
  * 내용 : Java 클래스 실습하기
+ * 
+ * 클래스와 객체
+ *  - 클래스는 객체를 생성하는 구조체이고 속성(필드)과 기능(메서드)으로 구성
+ *  - 객체는 클래스의 실제 인스터스이고 new 연산으로 생성
  */
 public class ClassTest {
 	public static void main(String[] args) {
@@ -44,8 +48,18 @@ public class ClassTest {
 		
 		kb.deposit(50000);
 		kb.withdraw(7000);
+		kb.balance--;
 		kb.show();
-			
-		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
