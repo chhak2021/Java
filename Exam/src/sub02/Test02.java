8ab80ec752d51c26c4c07619fa99e13c55b5b566
chhak0503 <chhak0503@gmail.com>
2022-09-02 17:02:26 +0900

    2022/09/02 연습문제3 완료

--- Exam/src/sub02/Test02.java
+++ Exam/src/sub02/Test02.java
@@ -1,22 +1,16 @@
 package sub02;
 
 public class Test02 {
+	
 	public static void main(String[] args) {
 		
 		int arr[] = { 17, 92, 18, 33, 58, 7, 26, 42 };
 				
-		
-		
-		int maxNum = arr[0];
-		
-		
+		int maxNum = arr[0];			
 
-		for (int i = 0; i < 8; i++) {
-			
-			if (maxNum < arr[i]) {
-				
-				maxNum = arr[i];
-				
+		for (int i = 0; i < 8; i++) {			
+			if (maxNum < arr[i]) {				
+				maxNum = arr[i];				
 			}
 		}
 
--- Exam/src/sub03/Test06.java
+++ Exam/src/sub03/Test06.java
@@ -1,5 +1,29 @@
 package sub03;
 
-public class Test06 {
-
+/*
+날짜 : OOOO/OO/OO
+이름 : 홍길동
+내용 : 클래스 상속 연습문제
+*/
+class Rent {
+	public void payment() {
+		System.out.println("임대료를 받습니다.");
+	}
 }
+
+class Landload extends Rent {
+	
+	public void getMoney() {
+		System.out.println("건물주 입니다.");
+		payment();
+	}
+}
+
+public class Test06 {
+	public static void main(String[] args) {	
+		Landload master = new Landload();
+		master.getMoney();		
+	}	
+}
+
+
